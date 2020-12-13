3f91ef1e0f48c9f034599006c1bf2c7e4f9fcf89
KarinaChuprakova <71293068+KarinaChuprakova@users.noreply.github.com>
2020-12-13 16:13:37 +0530

    Add files via upload

--- chisla_fibonacci.py
+++ chisla_fibonacci.py
@@ -1,1 +1,23 @@
+class Fibn:
+    class _Fibn_iter:
+        def __init__(self):
+            self.i = 0
+            self.fibs = chisla
+            
+        def __next__(self):
+                if self.i < 0:
+                    raise StopIteration()
+                else:
+ n=int(input())
+                    j = self.i
+                    self.i += 1
+                    return fibonacci(n - 1) + fibonacci(n - 2)
+        def __iter__(self):
+             return Fibn._Fibn_iter()
 
+fn = Fibn()
+
+for f in fn:
+    print(f)
+    
+print(list(fn))
