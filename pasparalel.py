2babd47d4876801b9396aa6dbb95b188a86a0e5f
KarinaChuprakova <71293068+KarinaChuprakova@users.noreply.github.com>
2021-04-03 19:18:37 +0300

    Update pasparalel.py

--- pasparalel.py
+++ pasparalel.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import random
-import time
-import sys
 import multiprocessing
+import random
 
 n = 10000
 
-def pi():
+def pi(z):
     in_circle = 0
     for i in range(n):
         x = random.uniform(-1, 1)
@@ -21,11 +19,10 @@ def test_all(pool):
     l = pool.map(pi, [1] * n)
     return l
 
-
 if __name__ == '__main__':
     pool = multiprocessing.Pool()
     print(test_all(pool))
     m = sum(test_all(pool))
-    print((4 * m) / n**2)
+    print((4 * m) / n)
 else:
     print(__name__)
